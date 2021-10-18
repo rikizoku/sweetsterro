c14ad5470761e4170b384599f4e24c11fb7b0346
blackstarss <kota.weeds1204@gmail.com>
2021-10-18 08:45:12 +0000

    [kota]管理者側会員一覧ページstart

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  
   devise_for :admins
     namespace :admin do
     resources :items
+    resources :customers
   end
   devise_for :customers
 
   get 'homes/about' => 'homes#about'
   root :to => "homes#top"
 
-  resource :customers
+ 
 
 
 end
