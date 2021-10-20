b35f852c027d2c7345d2552eee3584b2a570f3bb
blackstarss <kota.weeds1204@gmail.com>
2021-10-20 02:28:04 +0000

    [start]退会確認画面

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -18,10 +18,20 @@ class Customers::CustomersController < ApplicationController
   end
   
   def withdraw_confirm
+    
   end
   
+  protected
+  # 退会しているかを判断するメソッド
   def withdraw
-    
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: parms[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      ## 【処理内容3】
+    end
   end
   
   private
