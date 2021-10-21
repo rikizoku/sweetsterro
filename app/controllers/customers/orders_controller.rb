9dd9f4a93cad35dff4f88cfdbd803ff083ebfb96
fgk244 <fgk.244@gmail.com>
2021-10-21 10:21:04 +0000

    21-3UpdateOderNew

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,32 +1,29 @@
 class Customers::OrdersController < ApplicationController
 
-  def new
-  end
-
 
   #注文情報入力ページ
-  #def new
-    #@addresses = current_customer.address
-    #@address = Address.new
-    #@order = Order.new
-  #end
+  def new
+    @addresses = current_customer.addresses
+    @address = Address.new
+    @order = Order.new
+  end
   #注文情報入力ページ
 
  #注文情報入力ページ
-  #def create
-    #session[:payment] = params[:payment]
-    #if params[:select] == "select_address"
-      #session[:address] = params[:address]
-   #elsif params[:select] == "my_address"
-      #session[:address] ="〒" + current_customer.post_code+current_customer.address+current_customer.last_name+current_customer.first_name
-    #end
-    #if session[:address].present? && session[:payment].present?
-      #redirect_to confirm_orders_path
-    #else
-      #flash[:order_new] = "支払方法とお届け先を選択して下さい"
-      #redirect_to new_order_path
-    #end
-  #end
+  def create
+    session[:payment] = params[:payment]
+    if params[:select] == "select_address"
+       session[:address] = params[:address]
+    elsif params[:select] == "my_address"
+      session[:address] ="〒" + current_customer.postal_code+current_customer.address+current_customer.last_name+current_customer.first_name
+    end
+    if session[:address].present? && session[:payment].present?
+       redirect_to confirm_orders_path
+    else
+       flash[:order_new] = "支払方法とお届け先を選択して下さい"
+       redirect_to new_order_path
+    end
+  end
  #注文情報入力ページ
 
    def confirm
@@ -42,8 +39,12 @@ class Customers::OrdersController < ApplicationController
 
   private
 
+  def address_params
+     params.require(:address).permit(:last_name, :first_name, :post_code, :address)
+  end
+
   def order_params
-   params.require(:order).permit(:name, :address, :total_payment)
+   params.require(:order).permit(:name, :address, :total_payment,:payment_method)
   end
 
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,6 @@
 class Address < ApplicationRecord
-  
   belongs_to :customer
-  
-  validates :postal_code, presence: true, format: { with: /^\d{7}$ }　# 郵便番号（ハイフンなし7桁）
-  validates :last_name,  presence: true
-   validates :first_name, presence: true
-   validates :address, presence: true
-
+  validates :postal_code, presence: true #, format: {with: /^\d{7}$}　# 郵便番号（ハイフンなし7桁）
+  validates :name,  presence: true
+  validates :address, presence: true
 end
