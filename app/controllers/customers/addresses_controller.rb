1492964b8235c094c46526da2eec1b55d02f13de
EC2 Default User <ec2-user@ip-172-31-91-245.ec2.internal>
2021-10-21 10:18:59 +0000

    配送先編集更新

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,28 +1,45 @@
 class Customers::AddressesController < ApplicationController
 
-  def edit
-  end
+def index
   
-  def update
+end
+
+def show
+  
+end
+
+
+def create
+  @address = Address.new(address_params)
+  @address.customer_id = current_customer.id
+  if @address.save
+  redirect_to address_path
+  else
+  @address_new = Address.new
+  @addresses = current_customer.addresses
+  render :index
   end
+end
 
-   def create
-    @address = Address.new(address_params)
-    @address.customer_id = current_customer.id
-    if @address.save
-       redirect_to address_path
-    else
-       @address_new = Address.new
-       @addresses = current_customer.addresses
-       render :index
-    end
-   end
+def edit
 
-     private
+end
+
+def update
+  address = Addresses.find(params[:id])
+  address.update(address_params)
+  redirect_to addresses_path
+end
+
+def destroy
+    
+end
+
+private
 
 
-    def address_params
-      params.require(:address).permit(:last_name, :first_name, :post_code, :address)
-    end
+def address_params
+    params.require(:address).permit(:last_name, :first_name, :post_code, :address)
+end
 
 end
