a52b7a8890379532493650815bd887f957541c23
blackstarss <kota.weeds1204@gmail.com>
2021-10-25 05:34:45 +0000

    [update]ジャンル一覧ページ+ジャンル削除ボタン

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,14 +1,21 @@
 class Customers::ItemsController < ApplicationController
-    before_action :authenticate_customer!
-    def show
-        @item = Item.find(params[:id])
-        @cart_item = current_customer.cart_items.new
-    end
 
-    def index
-        @items = Item.all
-        @item_page = Item.page(params[:page]).reverse_order
-    end
+def show
+    @item = Item.find(params[:id])
+    @cart_item = current_customer.cart_items.new
+end
+
+def index
+    @items = Item.all
+    @item_page = Item.page(params[:page]).reverse_order
+end
+
+def genreitem
+    @genre = Genre.find(params[:id])
+    @items = @genre.items
+    
+    @item_page = Item.page(params[:page]).reverse_order
+end   
 
 end
 
