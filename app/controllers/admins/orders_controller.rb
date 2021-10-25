e9b77a6f49f2f69275a2f371450a5fb66790798e
fgk244 <fgk.244@gmail.com>
2021-10-25 09:01:34 +0000

    25-2Update注文履歴詳細

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -7,15 +7,25 @@ class Admins::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @orders = OrderItem.all
+    @orders = @order.order_items
     #@oder_items = @order_items(params[:id])
     #@total = @cart_items.inject(0) { |sum, item| sum + item.cart_items_price }
+  end
+
+  def update
+    order_item = Order.find(params[:id])
+    order_item.update(order_item_params)
+    redirect_to admins_orders_path(order.order_id)
   end
 
   private
 
   def order_params
      params.require(:order).permit(:customer_id, :address, :payment_method, :postage, :total_payment, :status)
+  end
+
+  def order_item_params
+    params.require(:order_item).permit(:status)
   end
 
 end
