5cb4c462c49f0add8717633cdba1a756f1b72a2d
blackstarss <kota.weeds1204@gmail.com>
2021-10-25 07:47:43 +0000

    [finish]ジャンル検索＋管理者側注文一覧ページ

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -8,12 +8,13 @@ end
 def index
     @items = Item.all
     @item_page = Item.page(params[:page]).reverse_order
+    @genres = Genre.all
 end
 
 def genreitem
     @genre = Genre.find(params[:id])
     @items = @genre.items
-    
+    @genres = Genre.all
     @item_page = Item.page(params[:page]).reverse_order
 end   
 
