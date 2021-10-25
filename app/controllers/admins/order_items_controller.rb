5bb7927f731df226de322624428a663c278ca64e
chiphikari <bumblebee.chain55@gmail.com>
2021-10-25 04:29:10 +0000

    10/25-1

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -1,2 +1,3 @@
 class Admins::OrderItemsController < ApplicationController
+  before_action :authenticate_admin!
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,5 @@
 class HomesController < ApplicationController
 
-  before_action :authenticate_customer!, except: [:top, :about]
-
   def top
     @items = Item.where(sale_status: "販売中")
     @genre = Genre.all
