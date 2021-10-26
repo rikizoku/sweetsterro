5437e5304108112f239055c14243fda917bf1c4b
EC2 Default User <ec2-user@ip-172-31-82-231.ec2.internal>
2021-10-26 05:35:05 +0000

    Fix

--- db/migrate/20211025084243_change_datatype_payment_method_to_orders.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDatatypePaymentMethodToOrders < ActiveRecord::Migration[5.2]
-
-  def change
-    change_column :orders, :payment_method, :integer, default: false
-  end
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_10_25_063800) do
-
+ActiveRecord::Schema.define(version: 2021_10_20_214333) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -90,8 +88,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_063800) do
   end
 
   create_table "orders", force: :cascade do |t|
-
-    t.integer "payment_method", default: 0
+    t.integer "payment_method"
     t.string "address"
     t.string "postal_code"
     t.string "name"
