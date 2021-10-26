d2bf87ac65de3d8a546e738ebdc7eae1a42f0879
chiphikari <bumblebee.chain55@gmail.com>
2021-10-26 09:06:06 +0000

    10/26-5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,22 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when Customer
-      root_path
+      customers_path
     when Admin
       admins_path
     end
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+        root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+        root_path
+    end
+  end
+
 
   protected
 
