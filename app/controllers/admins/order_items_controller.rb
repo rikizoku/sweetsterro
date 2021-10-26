cc001b7548c47f1360e38f18aab7e17377c38a41
chiphikari <bumblebee.chain55@gmail.com>
2021-10-26 07:32:10 +0000

    10/26-2

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -2,23 +2,10 @@ class Admins::OrderItemsController < ApplicationController
 
   before_action :authenticate_admin!
 
-
-  def update
-    order_item = OrderItem.find_by(params[:id])
-    order_item.update(order_item_params)
-    redirect_to admins_path
-  end
-
-   #def update
-    #order = Order.find(params[:id])
-    #order.update(order_params)
-    #redirect_to admins_ordesr_path(order)
-   #nd
-
-  private
-
-  def order_item_params
-    params.require(:order_item).permit(:order_id, :item_id, :production_status)
-  end
+   def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+    redirect_to admins_ordesr_path(order)
+   end
 
 end
